3599eaea98ebe2e94b64c6e8d39b4ee7c620b00d
Ajay Kumar <ajaykumarmkaju@gmail.com>
2022-03-16 22:31:29 +0530

    Added about

--- blog/urls.py
+++ blog/urls.py
@@ -20,4 +20,5 @@ from posts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,"posts/home.html")
+
+def about(request):
+    return render(request, "posts/about.html")
