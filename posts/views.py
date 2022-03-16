d63738cee7bb72c4fe7e8a517d76572106f07ddf
Ajay Kumar <ajaykumarmkaju@gmail.com>
2022-03-16 23:57:20 +0530

    Added Post Model

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from posts.models import Post
 
 def home(request):
-    return render(request,"posts/home.html")
+    posts = Post.objects.all()
+
+    return render(request,"posts/home.html",{'posts':posts})
 
 def about(request):
     return render(request, "posts/about.html")
